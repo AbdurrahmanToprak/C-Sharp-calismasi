0846a84de987474254021fbaf0f100cfc48cf872
Abdurrahman TOPRAK <toprakelz23@gmail.com>
2024-07-17 01:54:04 +0300

    math

--- MerhabaC#/Program.cs
+++ MerhabaC#/Program.cs
@@ -19,3 +19,11 @@ Console.Write("2.sayi : ");
 y = Convert.ToInt32(Console.ReadLine());
 Console.Write(x % y);
 
+//Matematik işlemleri
+
+Console.WriteLine(Math.Pow(2,4)); //us alma
+Console.WriteLine(Math.Sqrt(4)); //karekok
+Console.WriteLine(Math.Abs(4)); //0 a Uzaklık
+Console.WriteLine(Math.Round(4.3)); //Yuvarlama
+Console.WriteLine(Math.Min(4,3)); //min sayı
+Console.WriteLine(Math.Max(4,3)); //max sayı
